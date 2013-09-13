f5aeb61306b298b8eef73f8f4b922f64a3f125eb
kevlarmusik <kevlar.eq@gmail.com>
2013-09-13 16:10:24 -0400

    added rack-test

--- Gemfile
+++ Gemfile
@@ -1,7 +1,3 @@
 source 'https://rubygems.org'
 
-# Specify your gem's dependencies in rulers.gemspec
 gemspec
-
-source :rubygems
-    gem 'rulers'
--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,11 +1,11 @@
 require "rulers/version"
 require "rulers/array"
-    module Rulers
-      class Application
-        def call(env)
-        	`echo debug > debug.txt`;
-          [200, {'Content-Type' => 'text/html'},
-            ["Does this thing work?!?!"]]
-        end
-			end 
-		end
+module Rulers
+  class Application
+    def call(env)
+    	`echo debug > debug.txt`;
+      [200, {'Content-Type' => 'text/html'},
+        ["Does this thing work?!?!"]]
+    end
+	end 
+end
